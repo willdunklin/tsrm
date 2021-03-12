ffe92dc91db17f4f85940c57cba7eb411b3161fc
willdunklin <dunkliw@clarkson.edu>
2021-03-12 13:22:03 -0500

    command parsing

--- chat_reader.py
+++ chat_reader.py
@@ -8,3 +8,23 @@ bot = commands.Bot(
     prefix=os.environ['BOT_PREFIX'],
     initial_channels=[os.environ['CHANNEL']]
 )
+
+@bot.event
+async def event_ready():
+    print(f'{bot.nick} is up')
+    # ws = bot._ws
+    # await ws.send_privmsg(os.environ['CHANNEL'], f'/me pogu')
+
+@bot.event
+async def event_message(context):
+    print(context.content)
+    await bot.handle_commands(context)
+    if context.author.name.lower() == bot.nick.lower():
+        return
+    await context.channel.send(context.content)
+
+@bot.command(name='test')
+async def test(context):
+    await context.send('test passed pog')
+
+bot.run()
