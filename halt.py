3bf90552804026bb978991ba5bacaacd8a9464aa
willdunklin <dunkliw@clarkson.edu>
2021-03-14 14:18:49 -0400

    manual halt

---
+++ halt.py
@@ -0,0 +1,5 @@
+import keyboard
+
+def halt():
+    keyboard.wait('h')
+    print('halted')
--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 from threading import Thread
 import time
 
-import input_handler as ih
+from input_handler import instruction, unpress
 from chat import Chat
 import process_test
-
-def print_tike():
-    print(time.time())
+from halt import halt
 
 c = Chat()
-
 chat_thread = Thread(target=c.run)
 chat_thread.start()
 
+halt_thread = Thread(target=halt)
+halt_thread.start()
+
 try:
-    while True:
+    while halt_thread.is_alive():
         if not c.empty():
             command = c.pop()
             print('executing: ', command.lower())
-            execution = Thread(target=ih.instruction, args=(command.lower(),))
+            execution = Thread(target=instruction, args=(command.lower(),))
             execution.start()
             if not process_test.exists():
                 break
             time.sleep(0.05)
         else:
             time.sleep(0.5)
+            print(halt_thread.isAlive())
 
-    print('minecraft not running, exiting')
-    ih.unpress()
+    print('exiting')
+    unpress()
 
 except KeyboardInterrupt:
-    ih.unpress()
+    unpress()
     # this doesn't work, i dont know how to kill the thread the bot is in its own infinite loop
     print('unpressed keys, terminating thread')
     exit()
